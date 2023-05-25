64edaf9cd9a47476577cb5516a817f405574640f
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2023-05-25 21:46:05 +0200

    Update 17.c

--- SP/Exercises for second midterm and exams Quiz/17.c
+++ SP/Exercises for second midterm and exams Quiz/17.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// DO NOT CHANGE!
+int hexadecimal(char text[]) {
+    int i=0;
+    while (text[i] != 0) {
+        text[i] = toupper(text[i]);
+        if (text[i] == '\n') {
+            return 0;
+        }
+        if (isdigit(text[i]) == 0 &&
+            (
+                    text[i] != 'A' &&
+                    text[i] != 'B' &&
+                    text[i] != 'C' &&
+                    text[i] != 'D' &&
+                    text[i] != 'E' &&
+                    text[i] != 'F'
+            )) {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+
+}
+void wtf() {
+    FILE *f = fopen("input.txt", "w");
+    char c;
+    while ((c = getchar()) != EOF) {
+        fputc(c, f);
+    }
+    fclose(f);
+}
+
+int main()
+{
+    wtf();
+    char str[30];
+    FILE * f = fopen("input.txt", "r");
+    int count = 0;
+    while((fscanf(f, "%s", str)) != EOF) {
+        if (hexadecimal(str) == 1) {
+            puts(str);
+            count++;
+        }
+    }
+    printf("Total: %d", count);
+    fclose(f);
+    return 0;
+}
+
+
+
 
