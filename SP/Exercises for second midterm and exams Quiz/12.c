5489768d545096670b48ed348b96d22c9f79eda7
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2023-05-25 21:44:12 +0200

    Update 12.c

--- SP/Exercises for second midterm and exams Quiz/12.c
+++ SP/Exercises for second midterm and exams Quiz/12.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+int main(){
+    char text[100];
+    fgets(text, sizeof(text), stdin);
+    text[strlen(text)-1] = '\0';
+
+    char text2[100];
+    int first;
+    int second= strlen(text)-1;
+    int counter = 0;
+    for (int i = 0; i < strlen(text); ++i) {
+        if(isdigit(text[i])){
+            counter++;
+            if(counter==1){
+                first = i;
+            }
+            if(counter==2){
+                second=i;
+            }
+        }
+    }
+    if(counter == 0){
+        printf("No digits");
+    }else{
+        for (int i = first; i <= second; ++i) {
+            printf("%c", text[i]);
+        }
+    }
+    return 0;
+}
