d5f97704468c561b08f79873c9710e7c0d39c15f
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2023-05-25 20:22:35 +0200

    Update 01.c

--- SP/Exercises for second midterm and exams Quiz/01.c
+++ SP/Exercises for second midterm and exams Quiz/01.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+void transform(char str[100], int x, int i) {
+    if (str[i] == '\0') {
+        return;
+    } else {
+        if(isalpha(str[i])) {
+            if (isupper(str[i])) {
+                if (str[i] + x > 'Z') {
+                    str[i] = str[i] - ('Z' + 1) + 'A' + x;
+                } else {
+                    str[i] = str[i] + x;
+                }
+            }
+            if (islower(str[i])) {
+                if (str[i] + x > 'z') {
+                    str[i] = str[i] - ('z' + 1) + 'a' + x;
+                } else {
+                    str[i] = str[i] + x;
+                }
+            }
+        }
+        transform(str, x, i + 1);
+    }
+}
+
+int main() {
+    int n;
+    int x;
+    scanf("%d%d\n", &n, &x);
+
+    char str[100];
+    for (int i = 1; i <= n; ++i) {
+        fgets(str, sizeof(str), stdin);
+        str[strlen(str)-1] = '\0';
+        transform(str, x, 0);
+        puts(str);
+    }
+    return 0;
+}
 
