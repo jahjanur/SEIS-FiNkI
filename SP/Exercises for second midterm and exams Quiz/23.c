87b14f0f22c701e837dee6cd0845bb02b4f83213
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2023-05-25 21:48:47 +0200

    Update 23.c

--- SP/Exercises for second midterm and exams Quiz/23.c
+++ SP/Exercises for second midterm and exams Quiz/23.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void column(int start, int end) {
+    if (start > end) {
+        return;
+    } else {
+        printf("%d", start);
+        column(start + 1, end);
+    }
+}
+
+void triangle(int n) {
+    if (n == 0) {
+        return;
+    } else {
+        triangle(n - 1);
+        column(1, n);
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    triangle(n);
+
+    return 0;
+}
