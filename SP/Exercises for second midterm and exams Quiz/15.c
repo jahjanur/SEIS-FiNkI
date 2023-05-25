f027c878e1b86a6209b3802798135b0400d56f1f
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2023-05-25 21:45:01 +0200

    Update 15.c

--- SP/Exercises for second midterm and exams Quiz/15.c
+++ SP/Exercises for second midterm and exams Quiz/15.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include  <stdlib.h>
 
+
+int  main(){
+   int matrix[100][100];
+   int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    int counter = 0;
+    for (int i = 0; i < n; ++i) {
+        if (matrix[i][i] != 0) {
+            matrix[i][i] = 0;
+            counter++;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if(i==j) continue;
+            if(matrix[i][j] != matrix[j][i]){
+                matrix[i][j] = matrix[j][i];
+                counter++;
+            }
+        }
+    }
+    printf("%d", counter);
+
+    return 0;
+}
