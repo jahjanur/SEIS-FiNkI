2af8dce0cfc030b5657571fde6de6424e81374a8
Jahja Nur Zulbeari <92380280+jahjanur@users.noreply.github.com>
2023-05-25 21:44:27 +0200

    Update 13.c

--- SP/Exercises for second midterm and exams Quiz/13.c
+++ SP/Exercises for second midterm and exams Quiz/13.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include  <stdlib.h>
 
+void tranform(int array[], int n){
+    for (int i = 0; i < n; ++i) {
+        if(array[i]==array[i+1]){
+            array[i]=array[i] * 2;
+            array[i+1]= 0;
+        }
+    }
+}
+
+void reordering(int array[], int n){
+    int j=0;
+    for (int i = 0; i < n; ++i) {
+        if(array[i] != 0){
+            array[j]=array[i];
+            j++;
+        }
+    }
+    for (int k = j; k < n; ++j) {
+        array[j]=0;
+    }
+}
+int  main(){
+    int n;
+    int array[100];
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &array[i]);
+    }
+
+    tranform(array,n);
+    reordering(array, n);
+    for (int i = 0; i < n;++i) {
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
